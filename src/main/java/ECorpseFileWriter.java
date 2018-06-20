bc83ef93f4441ba576df0747edbf3b3f8c67069a
quelloggs <gtsallier@gmail.com>
2018-06-19 22:30:46 -0500

    added file writing to the file writer

--- src/main/java/ECorpseFileWriter.java
+++ src/main/java/ECorpseFileWriter.java
@@ -13,6 +13,7 @@ public class ECorpseFileWriter {
         String fileName = eCorpse.getTitle();
 
         File file = new File(fileName + ".txt");
+        file.createNewFile();
         FileWriter fileWriter = new FileWriter(file);
         fileWriter.write(eCorpse.getTitle());
         fileWriter.write(String.valueOf(eCorpse.getPoem().listIterator()));
